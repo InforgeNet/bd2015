6f89b1a9df759d658fe0bd61438fdae5198d1086
Niccolò Scatena <SpeedJack95@gmail.com>
2015-12-09 07:55:23 +0100

    check on comanda for 'nuova_consegna'

--- sql/triggers.sql
+++ sql/triggers.sql
@@ -2,7 +2,8 @@ DELIMITER $$
 
 /******************************************************************************
  * nuova_consegna si assicura che ogni volta che viene inserita una nuova     *
- * consegna il pony a cui viene assegnata sia libero.                         *
+ * consegna il pony a cui viene assegnata sia libero e che la comanda sia     *
+ * effettivamente in attesa di consegna.                                      *
  ******************************************************************************/
 CREATE TRIGGER nuova_consegna
 BEFORE INSERT
@@ -10,11 +11,19 @@ ON Consegna
 FOR EACH ROW
 BEGIN
     SET @StatoPony = (SELECT P.Stato AS StatoPony
-                        FROM Pony P WHERE P.id = new.Pony);
+                        FROM Pony P
+                        WHERE P.ID = new.Pony);
 
-    IF @StatoPony != "libero" THEN
+    SET @StatoConsegna = (SELECT C.stato AS StatoConsegna
+                                FROM Comanda C
+                                WHERE C.ID = new.ID);
+                                
+    IF @StatoPony <> 'libero' THEN
         SIGNAL SQLSTATE '45000'
-        SET MESSAGE_TEXT = 'Assegnamento di una consegna a un pony non libero.';
+        SET MESSAGE_TEXT = 'Assegnamento di consegna a pony non libero.';
+    ELSE IF @StatoConsegna <> 'consegna' THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Comanda relativa non pronta per la consegna.';
     END IF;
 END$$
 DELIMITER ;
