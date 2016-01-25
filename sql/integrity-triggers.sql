03ecb154f6166c4748ea06eca1f2a786269e4eab
Niccolò Scatena <SpeedJack95@gmail.com>
2016-01-25 15:52:50 +0100

    only integrity-related triggers are shown in ch4

--- sql/integrity-triggers.sql
+++ sql/integrity-triggers.sql
@@ -1,30 +1,15 @@
 DELIMITER $$
 
-CREATE TRIGGER nuovo_magazzino
-BEFORE INSERT
-ON Magazzino
-FOR EACH ROW
-BEGIN
-    -- Simula AUTO_INCREMENT
-    IF NEW.ID IS NULL THEN
-        SET NEW.ID = (SELECT IFNULL(MAX(ID), 0) + 1
-                            FROM Magazzino
-                            WHERE Sede = NEW.Sede);
-    END IF;
-END;$$
-
+/******************************************************************************
+ * nuova_confezione controlla che DataCarico, se presente, non sia precedente * 
+ * a DataAcquisto. Inoltre controlla che sia specificati tutti gli attributi  *
+ * necessari.                                                                 * 
+ ******************************************************************************/ 
 CREATE TRIGGER nuova_confezione
 BEFORE INSERT
 ON Confezione
 FOR EACH ROW
 BEGIN
-    -- Simula AUTO_INCREMENT
-    IF NEW.Numero IS NULL THEN
-        SET NEW.Numero = (SELECT IFNULL(MAX(Numero), 0) + 1
-                            FROM Confezione
-                            WHERE CodiceLotto = NEW.CodiceLotto);
-    END IF;
-                        
     IF NEW.DataCarico IS NOT NULL THEN
         IF NEW.DataCarico < NEW.DataAcquisto THEN
             SIGNAL SQLSTATE '45000'
@@ -49,6 +34,11 @@ BEGIN
     END IF;
 END;$$
 
+/******************************************************************************
+ * aggiorna_confezione controlla che DataCarico, se presente, non sia         * 
+ * precendente a DataAcquisto. Inoltre controlla che sia specificati tutti    *
+ * gli attributi necessari.                                                   * 
+ ******************************************************************************/ 
 CREATE TRIGGER aggiorna_confezione
 BEFORE UPDATE
 ON Confezione
@@ -79,6 +69,13 @@ BEGIN
     END IF;
 END;$$
 
+/******************************************************************************
+ * nuovo_menu contolla che il periodo di applicazione del nuovo menu inserito *
+ * non sia in conflitto con il periodo di applicazione di un altro menu nella *
+ * stessa sede (ogni sede applica un solo menu alla volta). Inoltre controlla *
+ * che DataFine sia successiva a DataInizio.                                  *
+ * Business Rule: (BR05)                                                      *
+ ******************************************************************************/
 CREATE TRIGGER nuovo_menu
 BEFORE INSERT
 ON Menu
@@ -227,33 +224,6 @@ BEGIN
     END IF;
 END;$$
 
-CREATE TRIGGER nuova_sala
-BEFORE INSERT
-ON Sala
-FOR EACH ROW
-BEGIN
-    -- Simula AUTO_INCREMENT
-    IF NEW.Numero IS NULL THEN
-        SET NEW.Numero = (SELECT IFNULL(MAX(Numero), 0) + 1
-                            FROM Sala
-                            WHERE Sede = NEW.Sede);
-    END IF;
-END;$$
-
-CREATE TRIGGER nuovo_tavolo
-BEFORE INSERT
-ON Tavolo
-FOR EACH ROW
-BEGIN
-    -- Simula AUTO_INCREMENT
-    IF NEW.Numero IS NULL THEN
-        SET NEW.Numero = (SELECT IFNULL(MAX(Numero), 0) + 1
-                            FROM Tavolo
-                            WHERE Sede = NEW.Sede
-                                AND Sala = NEW.Sala);
-    END IF;
-END;$$
-
 CREATE TRIGGER nuova_comanda
 BEFORE INSERT
 ON Comanda
@@ -282,66 +252,6 @@ BEGIN
         SIGNAL SQLSTATE '45000'
         SET MESSAGE_TEXT = 'Una comanda deve essere o da tavolo o take-away. '
                             'Non entrambe.';
-    END IF;
-END;$$
-
-CREATE TRIGGER assegna_pony
-AFTER UPDATE
-ON Piatto
-FOR EACH ROW
-BEGIN
-    DECLARE ComandaTakeAway BOOL;
-    DECLARE PiattiNonInServizio INT;
-    DECLARE SedeComanda VARCHAR(45);
-    DECLARE PonyScelto INT;
-    
-    IF NEW.Stato = 'servizio' THEN
-        SET ComandaTakeAway = (SELECT C.Account <> NULL
-                                FROM Comanda C
-                                WHERE C.ID = NEW.Comanda);
-                                
-        IF ComandaTakeAway THEN
-            SET PiattiNonInServizio = (SELECT COUNT(*)
-                                        FROM Piatto P
-                                        WHERE P.Comanda = NEW.Comanda
-                                            AND P.Stato <> 'servizio');
-            
-            IF PiattiNonInServizio = 0 THEN
-                -- Scegli Pony
-                SET SedeComanda = (SELECT C.Sede
-                                    FROM Comanda C
-                                    WHERE C.ID = NEW.Comanda);
-                
-                SET PonyScelto = (SELECT P.ID
-                                    FROM Pony P
-                                    WHERE P.Sede = SedeComanda
-                                        AND P.Stato = 'libero'
-                                        AND Ruote = (
-                                        (SELECT COUNT(*)
-                                        FROM Piatto P
-                                        WHERE P.Comanda = NEW.Comanda) > 5));
-                                        
-                IF PonyScelto = NULL THEN
-                    SET PonyScelto = (SELECT P.ID
-                                        FROM Pony P
-                                        WHERE P.Sede = SedeComanda
-                                        AND P.Stato = 'libero');
-                END IF;
-                
-                IF PonyScelto = NULL THEN
-                    -- Nessun Pony disponibile
-                    SIGNAL SQLSTATE '01000' -- Warning
-                    SET MESSAGE_TEXT = 'Nessun Pony è stato assegnato in '
-                                        'sono tutti occupati.';
-                ELSE
-                    -- Assegna Pony
-                    INSERT INTO Consegna(Comanda, Sede, Pony, Partenza, Arrivo,
-                                            Ritorno)
-                    VALUES (NEW.Comanda, SedeComanda, PonyScelto, NULL, NULL,
-                                NULL);
-                END IF;
-            END IF;
-        END IF;
     END IF;
 END;$$
 
@@ -463,6 +373,12 @@ BEGIN
     END IF;
 END;$$
 
+/******************************************************************************
+ * nuova_modifica controlla che, per il piatto sul quale si sta applicando la *
+ * variazione, non siano già state scelte 3 variazioni. Inoltre controlla che *
+ * la ricetta del piatto e quella della variazione corrispondano.             *
+ * Business Rule: (BR12)                                                      *
+ ******************************************************************************/
 CREATE TRIGGER nuova_modifica
 BEFORE INSERT
 ON Modifica
@@ -521,33 +437,10 @@ BEGIN
     END IF;
 END;$$
 
-CREATE TRIGGER nuovo_pony
-BEFORE INSERT
-ON Pony
-FOR EACH ROW
-BEGIN
-    -- Simula AUTO_INCREMENT
-    IF NEW.ID IS NULL THEN
-        SET NEW.ID = (SELECT IFNULL(MAX(ID), 0) + 1
-                            FROM Pony
-                            WHERE Sede = NEW.Sede);
-    END IF;
-END;$$
-
-CREATE TRIGGER nuova_consegna
-BEFORE INSERT
-ON Consegna
-FOR EACH ROW
-BEGIN
-    UPDATE Pony SET Stato = 'occupato'
-        WHERE Sede = NEW.Sede
-            AND ID = NEW.Pony;
-    
-    -- Le nuove consegne non devono avere mai Arrivo e Ritorno.
-    SET NEW.Arrivo = NULL;
-    SET NEW.Ritorno = NULL;
-END;$$
-
+/******************************************************************************
+ * aggiorna_consegna si assicura che l'arrivo registrato sia sempre successivo*
+ * alla partenza e che il ritorno sia sempre successivo all'arrivo.           *
+ ******************************************************************************/
 CREATE TRIGGER aggiorna_consegna
 BEFORE UPDATE
 ON Consegna
@@ -570,17 +463,12 @@ BEGIN
     END IF;
 END;$$
 
-CREATE TRIGGER libera_pony
-AFTER UPDATE
-ON Consegna
-FOR EACH ROW
-BEGIN
-    IF NEW.Ritorno IS NOT NULL THEN
-        UPDATE Pony SET Stato = 'libero' WHERE Sede = NEW.Sede
-                                            AND ID = NEW.Pony;
-    END IF;
-END;$$
-
+/******************************************************************************
+ * nuova_prenotazione controlla: se l'account (in caso di prenotazione online)*
+ * è abilitato a prenotare; se il tavolo da prenotare è libero; se la sala e  *
+ * tutti i tavoli che contiene sono liberi per un allestimento.               *
+ * Business Rule: (BR16) e (BR19)                                             *
+ ******************************************************************************/
 CREATE TRIGGER nuova_prenotazione
 BEFORE INSERT
 ON Prenotazione
@@ -626,34 +514,7 @@ BEGIN
             SET NEW.Descrizione = NULL;
             SET NEW.Approvato = NULL;
             
-            -- Assegna Tavolo
-            SET NEW.Tavolo = (SELECT T.Numero
-                                FROM Tavolo T
-                                WHERE T.Numero NOT IN (
-                                    SELECT P1.Tavolo
-                                    FROM Prenotazione P1
-                                    WHERE P1.Data >
-                                        (NEW.Data - INTERVAL 2 HOUR)
-                                        AND P1.Data <
-                                            (NEW.Data + INTERVAL 2 HOUR))
-                                    AND T.Sala NOT IN (
-                                        SELECT P2.Sala
-                                        FROM Prenotazione P2
-                                        WHERE P2.Data <
-                                            (NEW.Data - INTERVAL 2 HOUR)
-                                            AND P2.Data >
-                                                (NEW.Data + INTERVAL 2 HOUR)
-                                        AND P2.Tavolo = NULL)
-                                    AND T.Posti BETWEEN NEW.Numero
-                                                    AND (NEW.Numero + 3)
-                                ORDER BY T.Posti ASC
-                                LIMIT 1);
-                                    
-            IF NEW.Tavolo IS NULL THEN
-                SIGNAL SQLSTATE '45000'
-                SET MESSAGE_TEXT = 'Non ci sono tavoli liberi per questo '
-                                    'numero di persone.';
-            END IF;
+            -- Assegna Tavolo (riportato solo nello script finale nel Cap. 9)
             
         ELSEIF NEW.Descrizione IS NULL THEN
             SIGNAL SQLSTATE '45000'
@@ -672,6 +533,9 @@ BEGIN
         END IF;
     END IF;    
 
+-- NOTA: L'uso di espressioni booleane all'interno di SUM() è possibile solo in
+--       MySQL (che converte l'espressione booleana in int). In altri DBMS si
+--       può utilizzare COUNT() e spostare l'espressione booleana nel WHERE.
     SET AllestimentiSala = (SELECT SUM(DATE(P.Data) = DATE(NEW.Data))
                                 FROM Prenotazione P
                                 WHERE P.Sala = NEW.Sala
@@ -718,6 +582,12 @@ BEGIN
     END IF;
 END;$$
 
+/******************************************************************************
+ * aggiorna_prenotazione controlla che la rettifica della prenotazione venga  *
+ * fatta al max. 48 ore prima della data della prenotazione. Inoltre controlla*
+ * che la nuova prenotazione sia valida.                                      *
+ * Business Rule: (BR16) e (BR17)                                             *
+ ******************************************************************************/
 CREATE TRIGGER aggiorna_prenotazione
 BEFORE UPDATE
 ON Prenotazione
@@ -816,6 +686,11 @@ BEGIN
     END IF;
 END;$$
 
+/******************************************************************************
+ * elimina_prenotazione controlla che l'annullamento della prenotazione       *
+ * venga fatta al max. 72 ore prima della data della prenotazione.            *
+ * Business Rule: (BR18)                                                      *
+ ******************************************************************************/
 CREATE TRIGGER elimina_prenotazione
 BEFORE DELETE
 ON Prenotazione
@@ -879,13 +754,6 @@ BEGIN
     IF NEW.Efficienza < 1 OR NEW.Efficienza > 5 THEN
         SIGNAL SQLSTATE '45000'
         SET MESSAGE_TEXT = 'Efficienza deve essere compreso tra 1 e 5';
-    END IF;
-    
-    -- Simula AUTO_INCREMENT
-    IF NEW.Numero IS NULL THEN
-        SET NEW.Numero = (SELECT IFNULL(MAX(Numero), 0) + 1
-                            FROM Risposta
-                            WHERE Domanda = NEW.Domanda);
     END IF;
 END;$$
 
