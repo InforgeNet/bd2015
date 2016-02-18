ea6d9c3be9ec8e80779afa2428eb3e00b61d97fb
Niccolò Scatena <SpeedJack95@gmail.com>
2016-02-18 15:27:23 +0100

    added analytics 5

---
+++ sql/analytics-5.sql
@@ -0,0 +1,51 @@
+CREATE TABLE Report_TakeAway
+(
+    Posizione               INT UNSIGNED NOT NULL,
+    Sede                    VARCHAR(45) NOT NULL,
+    Pony                    INT UNSIGNED NOT NULL,
+    DeltaTempoAndata        TIME NOT NULL,
+    DeltaTempoRitorno       TIME NOT NULL,
+    PRIMARY KEY (Posizione),
+    UNIQUE KEY (Sede, Pony),
+    FOREIGN KEY (Sede, Pony)
+        REFERENCES Pony(Sede, ID)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+) ENGINE = InnoDB;
+
+CREATE EVENT aggiorna_Report_TakeAway
+ON SCHEDULE
+EVERY 1 WEEK
+STARTS TIMESTAMP(CURRENT_DATE) + INTERVAL 1 DAY + INTERVAL 3 HOUR
+ON COMPLETION PRESERVE
+DO
+BEGIN
+    DECLARE TempoMedioAndata INT;
+    DECLARE TempoMedioRitorno INT;
+    
+    TRUNCATE TABLE Report_TakeAway;
+    
+    SELECT CEIL(AVG(TIMESTAMPDIFF(SECOND, C.Partenza, C.Arrivo))) AS TMAndata,
+            CEIL(AVG(TIMESTAMPDIFF(SECOND, C.Arrivo, C.Ritorno))) AS TMRitorno
+        INTO TempoMedioAndata, TempoMedioRitorno
+    FROM Consegna C
+    WHERE C.Ritorno IS NOT NULL;
+    
+    SELECT @row_number := @row_number + 1 AS Posizione, D.*
+    FROM (SELECT @row_number := 0) AS N,
+        (
+            SELECT P.Sede, P.ID AS Pony,
+                    SEC_TO_TIME(
+                        CEIL(AVG(TIMESTAMPDIFF(SECOND, C.Partenza, C.Arrivo))) -
+                        TempoMedioAndata
+                        ) AS DeltaTempoAndata,
+                    SEC_TO_TIME(
+                        CEIL(AVG(TIMESTAMPDIFF(SECOND, C.Arrivo, C.Ritorno))) -
+                        TempoMedioRitorno
+                        ) AS DeltaTempoRitorno
+            FROM Pony P INNER JOIN Consegna C
+            GROUP BY P.Sede, P.ID
+        ) AS D
+    INTO Report_TakeAway
+    ORDER BY (D.DeltaTempoAndata + D.DeltaTempoRitorno) ASC;
+END;$$
