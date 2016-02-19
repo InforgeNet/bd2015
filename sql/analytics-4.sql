e4645a9346ea4628fc158f8640a4cceb69e53498
Niccolò Scatena <SpeedJack95@gmail.com>
2016-02-19 12:42:30 +0100

    completed analytics 4

--- sql/analytics-4.sql
+++ sql/analytics-4.sql
@@ -1,11 +1,9 @@
 CREATE TABLE Report_Sprechi
 (
-    Posizione               INT UNSIGNED NOT NULL,
     Sede                    VARCHAR(45) NOT NULL,
     Ingrediente             VARCHAR(45) NOT NULL,
     Spreco                  INT UNSIGNED NOT NULL,
-    PRIMARY KEY (Posizione),
-    UNIQUE KEY (Sede, Ingrediente),
+    PRIMARY KEY (Sede, Ingrediente),
     FOREIGN KEY (Sede)
         REFERENCES Sede(Nome)
         ON DELETE CASCADE
@@ -21,17 +19,6 @@ DELIMITER $$
 CREATE PROCEDURE AnalizzaSprechi(IN inizio TIMESTAMP, IN fine TIMESTAMP)
 NOT DETERMINISTIC MODIFIES SQL DATA
 BEGIN
-    DECLARE NomeSede VARCHAR(45);
-    DECLARE NomeRicetta VARCHAR(45);
-    DECLARE Numero INT;
-    DECLARE Finito1 BOOL DEFAULT FALSE;
-    DECLARE curSedeRicetta CURSOR FOR
-        SELECT C.Sede, P.Ricetta, COUNT(*) AS Numero
-        FROM Piatto P INNER JOIN Comanda C ON P.Comanda = C.ID
-        WHERE C.`Timestamp` BETWEEN inizio AND fine
-        GROUP BY C.Sede, P.Ricetta;
-    DECLARE CONTINUE HANDLER FOR NOT FOUND SET Finito1 = TRUE; 
-    
     IF inizio IS NULL THEN
         SET inizio = '1970-01-01 00:00:01';
     END IF;
@@ -39,47 +26,62 @@ BEGIN
         SET fine = CURRENT_TIMESTAMP;
     END IF;
     
-    OPEN curSedeRicetta;
+    TRUNCATE TABLE Report_Sprechi;
     
-    loop1_lbl: LOOP
-        FETCH curSedeRicetta INTO NomeSede, NomeRicetta, Numero;
-        IF Finito1 THEN
-            LEAVE loop1_lbl;
-        END IF;
+    BEGIN
+        DECLARE NomeSede VARCHAR(45);
+        DECLARE NomeIngrediente VARCHAR(45);
+        DECLARE Scaricata INT;
+        DECLARE Quantita INT;
+        DECLARE Finito BOOL DEFAULT FALSE;
+        DECLARE curScarichi CURSOR FOR
+            SELECT SL.Sede, SL.Ingrediente, COALESCE(SUM(SL.Quantita), 0) AS Qta
+            FROM Scarichi_Log SL
+            WHERE SL.`Timestamp` BETWEEN inizio AND fine;
+        DECLARE CONTINUE HANDLER FOR NOT FOUND SET Finito = TRUE;
         
-        BEGIN -- vanno considerate anche le variazioni
-            DECLARE NomeIngrediente VARCHAR(45);
-            DECLARE Quantita INT;
-            DECLARE Scaricata INT;
-            DECLARE Finito2 BOOL DEFAULT FALSE;
-            DECLARE curIngredienti CURSOR FOR
-                SELECT F.Ingrediente,
-                        COALESCE(SUM(F.Dose), 0)*Numero AS Quantita
-                FROM Fase F
-                WHERE F.Ricetta = NomeRicetta AND F.Ingrediente IS NOT NULL
-                GROUP BY F.Ingrediente; 
-            DECLARE CONTINUE HANDLER FOR NOT FOUND SET Finito2 = TRUE; 
+        OPEN curScarichi;
+        
+        loop_lbl: LOOP
+            FETCH curScarichi INTO NomeSede, NomeIngrediente, Scaricata;
+            IF Finito THEN
+                LEAVE loop_lbl;
+            END IF;
             
-            loop2_lbl: LOOP
-                FETCH curIngredienti INTO NomeIngrediente, Quantita;
-                IF Finito2 THEN
-                    LEAVE loop2_lbl;
-                END IF;
-                
-                SET Scaricata = (SELECT COALESCE(SUM(SL.Quantita), 0)
-                                FROM Scarichi_Log SL
-                                WHERE SL.Sede = NomeSede
-                                    AND SL.Ingrediente = NomeIngrediente
-                                    AND SL.`Timestamp` BETWEEN inizio AND fine);
-                                    
-                -- NON VA BENE L'INTERO CODICE: deve essere presa la quantità
-                -- di ingrediente usata per fare tutte le ricette, non una
-                -- alla volta.
-            END LOOP loop2_lbl;
-        END;
-    END LOOP loop1_lbl;
-    
-    CLOSE curSedeRicetta
+            SET Quantita = (
+            SELECT SUM(F.Dose) AS Quantita
+            FROM Fase F
+                INNER JOIN Ricetta R ON F.Ricetta = R.Nome
+                INNER JOIN Piatto P ON R.Nome = P.Ricetta
+                INNER JOIN Comanda C ON P.Comanda = C.ID
+            WHERE C.Sede = NomeSede
+                AND F.Ingrediente = NomeIngrediente
+                AND C.`Timestamp` BETWEEN inizio AND fine
+                AND F.ID NOT IN (SELECT MF.FaseVecchia
+                                FROM ModificaFase MF
+                                    INNER JOIN Variazione V
+                                        ON MF.Variazione = V.ID
+                                    INNER JOIN
+                                    (SELECT M.Variazione
+                                    FROM Modifica M
+                                    WHERE M.Piatto = P.ID) AS D
+                                        ON V.ID = D.Variazione)
+                AND F.ID NOT IN (SELECT MFN.FaseNuova
+                                FROM ModificaFase MFN
+                                    INNER JOIN Variazione VA
+                                        ON MFN.Variazione = VA.ID
+                                WHERE VA.ID NOT IN (SELECT MO.Variazione
+                                                    FROM Modifica MO
+                                                    WHERE MO.Piatto = P.ID))                                        
+            );
+            
+            INSERT INTO Report_Sprechi(Sede, Ingrediente, Spreco)
+            VALUES (NomeSede, NomeIngrediente, Scaricata - Quantita);
+            
+        END LOOP loop_lbl;
+        
+        CLOSE curScarichi;
+    END;
     
 END;$$  
 
